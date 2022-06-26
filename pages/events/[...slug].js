fc93fa7b6dea9692e493b11ce3203b5f2eb5ae79
akdlsz21 <akdlsz21@gmail.com>
2022-06-26 21:15:01 +0900

    added filter functionality

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,4 +1,6 @@
 import { useRouter } from 'next/router';
+import { getFilteredEvents } from '../../dummy-data';
+import EventList from '../../components/events/EventList';
 
 function FilteredEventsPage() {
 	const router = useRouter();
@@ -9,12 +11,30 @@ function FilteredEventsPage() {
 		return <p className="center">No filter data</p>;
 	}
 
-	return (
-		<div>
-			<h1>Events</h1>
-			<div> FilteredEvents</div>
-		</div>
-	);
+	const filteredYears = filterData[0];
+	const filteredMonth = filterData[1];
+
+	const numYear = Number(filteredYears);
+	const numMonth = Number(filteredMonth);
+
+	if (
+		isNaN(numYear) ||
+		isNaN(numMonth) ||
+		numYear > 2030 ||
+		numYear < 1970 ||
+		numMonth > 12 ||
+		numMonth < 1
+	) {
+		return <p className="center">Invalid filter data</p>;
+	}
+
+	const filteredEvents = getFilteredEvents({ year: numYear, month: numMonth });
+
+	if (!filteredEvents || filteredEvents.length === 0) {
+		return <p className="center">No events found</p>;
+	}
+
+	return <EventList items={filteredEvents} />;
 }
 
 export default FilteredEventsPage;
